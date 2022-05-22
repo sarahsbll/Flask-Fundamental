d23fec4fadc478c34a21ed6a348e31f5ad2e5092
sarahsbll <cynicoder@gmail.com>
2022-05-22 14:37:42 +0100

    test pygithub 3

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import dropbox
 # using an access token
 dbx = dropbox.Dropbox('sl.BIHrA2OxmhhRbKbeVGMAvWeCB-5V6jIugXVEN7ldnz0Gjp7QAbR2W0PhsiW0op9-fJllfeRXgWbRxYkxrDblx5MihNLvghWiIJYVWHu_QjoiVtPPeqUsye40a5Dm_cW3IaKl-8qRf-Q')
 for entry in dbx.files_list_folder('').entries:
-    print(entry.name)
+	print(entry.name)
 
 
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-	print("Data received from Webhook is: ", request.json)
-	#with open("test.txt", "w") as outfile:            
-		#outfile.write("test")
-		#outfile.close()
-        return "Webhook received!"
+	if request.method == 'POST':
+		print("Data received from Webhook is: ", request.json)
+		#with open("test.txt", "w") as outfile:            
+			#outfile.write("test")
+			#outfile.close()
+		return "Webhook received!"
 
 
 app.run(host='0.0.0.0', port=8000)
